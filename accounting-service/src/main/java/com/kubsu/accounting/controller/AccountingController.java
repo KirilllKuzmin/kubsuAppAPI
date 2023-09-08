eab40401f30467bd7a82a6668e17019ed194f9b2
Kirill Kuzmin <kirillkuzminr@gmail.com>
2023-09-09 02:17:33 +0300

    Implemented protection for account-service, and also added a couple of methods for testing

---
+++ accounting-service/src/main/java/com/kubsu/accounting/controller/AccountingController.java
@@ -0,0 +1,36 @@
+package com.kubsu.accounting.controller;
+
+import com.kubsu.accounting.service.UserDetailsImpl;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounting")
+public class AccountingController {
+
+
+    @GetMapping("/access")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("")
+    @PreAuthorize("hasRole('STUDENT') or hasRole('LECTURER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public UserDetailsImpl profile() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return userDetails;
+    }
+
+    @GetMapping("/lecturer")
+    @PreAuthorize("hasRole('LECTURER')")
+    public String lecturerAccess() {
+        return "Lecturer access.";
+    }
+}
